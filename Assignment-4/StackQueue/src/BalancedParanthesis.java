8e5aac0198f00068a0ed7836f4a463d64db91f4a
Abhik <abhikm135@gmail.com>
2021-02-20 12:05:37 +0530

    Balanced Paranthesis

---
+++ Assignment-4/StackQueue/src/BalancedParanthesis.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+import java.util.Scanner;
+
+public class BalancedParanthesis {
+    static boolean match(char a, char b){
+        return ((a == ')' && b == '(') || (a == '}' && b == '{')
+        || (a == ']' && b == '['));
+    }
+    static boolean isBalanced(String exp){
+        Stack<Character> s = new Stack<Character>();
+        for(int i=0; i<exp.length(); i++){
+            if( exp.charAt(i) == '(' || exp.charAt(i) == '{' || exp.charAt(i) == '['){
+                s.push(exp.charAt(i));
+            }
+            else{
+                if (s.isEmpty())
+                    return false;
+                else if (!match(exp.charAt(i), s.peek()))
+                    return false;
+                else
+                    s.pop();
+            }
+        }
+        return s.isEmpty();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String exp = sc.next();
+        System.out.println(isBalanced(exp));
+    }
+}
